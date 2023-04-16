31cf9db0b889f3c0db987b6d7b221a501ace663a
Juan Gilsanz Polo <juangilsanz@gmail.com>
2023-04-17 01:49:06 +0200

    Linux compatibility and other improvements

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/juan/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/juan/my_server_status" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.1+8" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 8 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/juan/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/juan/my_server_status"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/juan/my_server_status/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/juan/my_server_status/lib/main.dart"
+)
